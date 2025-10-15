4decf2ee598716aefe0fa28573c0a7d8ff4416ee
Dias62727 <168710665+Dias62727@users.noreply.github.com>
2025-10-15 22:20:20 +0200

    Update login.php to improve user authentication

--- login.php
+++ login.php
@@ -4,12 +4,11 @@ session_start();
 $users = array(
     "okxn" => "okxn123",
     "qwerty" => "qwerty123",
-    // Info o autorze itd.
-); 
+);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
 
     if (array_key_exists($username, $users) && $users[$username] === $password) {
         $_SESSION['loggedin'] = true;
@@ -17,8 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header("Location: dashboard.php");
         exit;
     } else {
-        echo "Invalid auth key.";
+        echo "Invalid username or password. Please try again.";
     }
+} else {
+    echo "Invalid request method.";
 }
-?>
-
